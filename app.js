121aff76c34d49f7cb40603e3df469fd393704e2
Natalia Aleksandrova <natalia.aleksandrova@crplab.ru>
2022-05-16 20:52:05 -0500

    test

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-//const express = require('express')
-import express from 'express';
-//const config = require('config')
-import config from 'config';
-//const mongoose = require('mongoose')
-import mongoose from 'mongoose';
+
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
-
 app.use('/api/auth', require('./routes/auth.routes'))
 
 const PORT = config.get('port') || 5000
 
-
 async function start() {
     try{
-        await mongoose.connect(config.get('mongoUri'), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        app.listen(PORT, () => console.log('App has been started on port 5000...'))
-    } catch(e) {
-        console.log('Server Error', e.message),
-        process.exit(code, 1)
-    }
+       await mongoose.connect(config.get ('mongoUri'), {
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+           //useCreateIndex: true
+          
+       })
+       app.listen(PORT, () => console.log('App worked'))
+   } catch (e){
+       console.log('Server Error', e.message)
+       process.exit(1)
+   }
 }
-
 start()
 
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,12 @@
 const {Router} = requare('express')
 const router = Router()
 
+router.post('/regicter', async(req, res) => {
+
+})
+
+router.post('/login', async(req, res) => {
+
+})
 
 module.exports = router
