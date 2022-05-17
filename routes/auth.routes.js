14c21e90bb33d1a732a793b3d09cedd03dae8c95
Natalia Aleksandrova <natalia.aleksandrova@crplab.ru>
2022-05-16 21:10:42 -0500

    Auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,11 @@ const {Router} = requare('express')
 const router = Router()
 
 router.post('/regicter', async(req, res) => {
-
+    try{
+        const{email, password} = req.body
+    } catch(e){
+        res.status(500).json({message: 'Что-то пошло не так'})
+    }
 })
 
 router.post('/login', async(req, res) => {
